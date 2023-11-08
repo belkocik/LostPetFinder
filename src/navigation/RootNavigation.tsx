5e7f5d9fbc1fb690ab49f8117b714ed8da143837
Marcin <belkocik97@interia.pl>
2023-11-08 22:56:52 +0100

    configure react navigation and absolute paths

---
+++ src/navigation/RootNavigation.tsx
@@ -0,0 +1,28 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+type Props = {};
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigation = (_props: Props) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
