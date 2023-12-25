ebba2d06ca3789113932f00984f8cf6127a49359
Marcin Niedbalec <belkocik97@interia.pl>
2023-12-25 16:18:38 +0100

    feat: login screen

--- src/features/auth/screens/stack/AuthStackScreen.tsx
+++ src/features/auth/screens/stack/AuthStackScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from '../LoginScreen';
+import {useTranslation} from 'react-i18next';
 
 const AuthStack = createNativeStackNavigator();
 
 const AuthStackScreen = () => {
+  const {t} = useTranslation();
   return (
     <AuthStack.Navigator>
-      <AuthStack.Screen name="Login" component={LoginScreen} />
+      <AuthStack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerTitle: t('loginHeader')}}
+      />
     </AuthStack.Navigator>
   );
 };
